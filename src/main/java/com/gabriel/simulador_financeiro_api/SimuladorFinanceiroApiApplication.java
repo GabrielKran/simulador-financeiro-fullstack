ecfeb4c5562c8cdd8fb6f8cb42a5319f56492ce3
GabrielKran <gabrielkran@gmail.com>
2025-11-19 13:33:12 -0300

    feat: inicializada a estrutuda basica do projeto Spring Boot

---
+++ src/main/java/com/gabriel/simulador_financeiro_api/SimuladorFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.simulador_financeiro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorFinanceiroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/simulador_financeiro_api/SimuladorFinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.simulador_financeiro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorFinanceiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
