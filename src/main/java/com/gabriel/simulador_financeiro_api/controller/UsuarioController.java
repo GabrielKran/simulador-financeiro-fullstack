d64911b618e4cb2dda26fa39557c64f9469391fc
GabrielKran <gabrielkran@gmail.com>
2025-11-24 12:52:09 -0300

    feat: adição de @CrossOrigin

--- src/main/java/com/gabriel/simulador_financeiro_api/controller/UsuarioController.java
+++ src/main/java/com/gabriel/simulador_financeiro_api/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.gabriel.simulador_financeiro_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.gabriel.simulador_financeiro_api.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
