004cf16c6aa8e2b26d361569b4bd4c20010dfd52
GabrielKran <gabrielkran@gmail.com>
2025-11-24 13:15:05 -0300

    feat: Adicioado o CrossOrigin(origins = "*")

--- src/main/java/com/gabriel/simulador_financeiro_api/controller/PlanoFinanceiroController.java
+++ src/main/java/com/gabriel/simulador_financeiro_api/controller/PlanoFinanceiroController.java
@@ -3,6 +3,7 @@ package com.gabriel.simulador_financeiro_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.gabriel.simulador_financeiro_api.entity.PlanoFinanceiro;
 import com.gabriel.simulador_financeiro_api.repository.PlanoFinanceiroRepository;
 @RestController
 @RequestMapping("/planos-financeiros")
+@CrossOrigin(origins = "*")
 public class PlanoFinanceiroController {
     @Autowired
     private PlanoFinanceiroRepository repository;
