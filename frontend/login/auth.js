0528aaa54a27db8df56803291537b222ec173693
GabrielKran <gabrielkran@gmail.com>
2026-01-06 14:36:31 -0300

    Arrumado a estrutura de pastas fazendo o index.html ficar solto

--- frontend/login/auth.js
+++ frontend/login/auth.js
@@ -65,7 +65,7 @@ async function fazerLogin() {
             localStorage.setItem("token", token);
             localStorage.setItem("usuario", usuario);
 
-            window.location.href = "/frontend/main/index.html";
+            window.location.href = "/frontend/index.html";
             
         } else {
             alert("Email ou senha inválidos");
